d0c44f3d8b1e6135205472aaeefe01b4b936a822
root <root@nicolas_server.home>
2019-01-14 15:47:35 +0100

    commentaire fichier + gestion parc

--- application.js
+++ application.js
@@ -1,8 +1,12 @@
-var mqtt = require('mqtt'); //https://www.npmjs.com/package/mqtt
-var Topic = '#'; //subscribe to all topics
-var Broker_URL = 'mqtt://ec2-35-180-11-57.eu-west-3.compute.amazonaws.com';
-var Database_URL = 'ec2-35-180-11-57.eu-west-3.compute.amazonaws.com';
+var mqtt = require('mqtt'); 	//https://www.npmjs.com/package/mqtt
+// Souscription à tous les topics
+var Topic = '#'; 		
+// URL du broker MQTT
+var Broker_URL = 'mqtt://127.0.0.1';
+// URL de la base de données
+var Database_URL = '127.0.0.1';
 
+// Options de connexion au broker MQTT
 var options = {
 	clientId: 'MyMQTT',
 	port: 1883,
@@ -11,6 +15,9 @@ var options = {
 	keepalive : 60
 };
 
+////////////////////////////////////////////////////
+///////////////////// MQTT /////////////////////////
+////////////////////////////////////////////////////
 var client  = mqtt.connect(Broker_URL, options);
 client.on('connect', mqtt_connect);
 client.on('reconnect', mqtt_reconnect);
@@ -18,24 +25,31 @@ client.on('error', mqtt_error);
 client.on('message', mqtt_messsageReceived);
 client.on('close', mqtt_close);
 
+// Fonction de connexion MQTT
 function mqtt_connect() {
-    console.log("Connecting MQTT");
+    console.log("Connexion MQTT");
+// Appel de la fonction de souscription
     client.subscribe(Topic, mqtt_subscribe);
 };
 
+// Fonction souscription à un topic
 function mqtt_subscribe(err, granted) {
-    console.log("Subscribed to " + Topic);
+    console.log("Subscribed à " + Topic);
     if (err) {console.log(err);}
 };
 
+// Fonction de reconnexion
 function mqtt_reconnect(err) {
-    console.log("Reconnect MQTT");
+    console.log("Reconnexion MQTT");
     //if (err) {console.log(err);}
+
+// Rappel de la fonction de connexion
 	client  = mqtt.connect(Broker_URL, options);
 };
 
+// Appel de la fonction d'erreur 
 function mqtt_error(err) {
-    console.log("Error!");
+    console.log("Erreur!");
 	//if (err) {console.log(err);}
 };
 
@@ -43,35 +57,30 @@ function after_publish() {
 	//do nothing
 };
 
-//receive a message from MQTT broker
+// Réception d'un message depuis le broker MQTT
 function mqtt_messsageReceived(topic, message, packet) {
 	var message_str = message.toString(); //convert byte array to string
 	message_str = message_str.replace(/\n$/, ''); //remove new line
 	//payload syntax: clientID,topic,message
-/*	if (countInstances(message_str) != 1) {
-		console.log("Invalid payload");
-	} else {	
-		insert_message(topic, message_str, packet);
-		//console.log(message_arr);
-	}
-*/
+
 	if (countInstances(message_str) == 3) {
 		insert_message(topic, message_str, packet);
 		//console.log(message_str);
-	} else {	
-		console.log("Invalid payload");
+	} else {
+		insert_message_parc(topic,message_str, packet);	
+		console.log("Invalide payload");
 	}
 };
 
 function mqtt_close() {
-	//console.log("Close MQTT");
+	//console.log("Fermeture MQTT");
 };
 
 ////////////////////////////////////////////////////
 ///////////////////// MYSQL ////////////////////////
 ////////////////////////////////////////////////////
 var mysql = require('mysql'); //https://www.npmjs.com/package/mysql
-//Create Connection
+// Création de la connexion
 var connection = mysql.createConnection({
 	host: Database_URL,
 	user: "user",
@@ -82,31 +91,111 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
 	if (err) throw err;
-	//console.log("Database Connected!");
+	//console.log("Connexion de la base de données !");
 });
 
-//insert a row into the tbl_messages table
+// Insertion d'une ligne dans la table tbl_message
 function insert_message(topic, message_str, packet) {
-	var message_arr = extract_string(message_str); //split a string into an array
-	
+	// Séparation de la string dans un tableau
+	var message_arr = extract_string(message_str); 
+	// Récupération du type
 	var type = message_arr[0];
+	// Récupération du numéro de série
 	var serialNumber = message_arr[1];
+	// Récupération de la mesure
 	var mesure = message_arr[2];
+	// Récupération de la localisation
 	var location = message_arr[3];
-	
+
+	// Requête SQL pour insérer les informations de la sonde en base de données
 	var sql = "INSERT INTO ?? (??,??,??,??,??) VALUES (?,?,?,?,?)";
-	var params = ['Tbl_message', 'topic','type', 'serialNumber', 'measurement', 'location' , topic, type, serialNumber, mesure, location];
+	var params = ['tbl_message', 'topic','type', 'serialNumber', 'measurement', 'location' , topic, type, serialNumber, mesure, location];
 	sql = mysql.format(sql, params);	
 	
 	connection.query(sql, function (error, results) {
 		if (error) throw error;
-		console.log("Message added: " + message_str);
+		console.log("Message ajouté dans la table tbl_message " + message_str);
 	}); 
+
+	// Requête SQL pour mettre à jour le statut du capteur (statut = 1 s'il n'est pas en défault)
+        var sql = "UPDATE ?? SET ?? = 1 WHERE ?? = ?";
+        var params = ['parc', 'status','serialNumber', serialNumber];
+        sql = mysql.format(sql, params);
+
+        connection.query(sql, function (error, results) {
+                if (error) throw error;
+                console.log("Mise à jour de la table parc " + message_str + "\n");
+        });
+
 };	
+
+// Insertion d'une ligne dans la table parc
+function insert_message_parc(topic, message_str, packet) {
+        var message_arr = extract_string(message_str); 
+
+        var type = message_arr[0];
+        var serialNumber = message_arr[1];
+        var mesure = message_arr[2];
+	// Variable utile dans le cas ou l'on transmet la localisation par MQTT (ici location=default)
+        var location = message_arr[3];
+	
+	var status = 0;
+	// Récupération de la date d'aujourd'hui
+	var dAujourdhui = new Date();
+	
+	// Requête SQL pour savoir si la sonde en question est déjà en défault (présente dans la base de données)
+	var rows_result_select;
+	var sql_insert = "SELECT ?? FROM ?? WHERE ?? = ?";
+        var params_insert = ['serialNumber', 'parc', 'serialNumber',serialNumber];
+	sql_insert = mysql.format(sql_insert, params_insert);
+	
+        connection.query(sql_insert, function (error, rows, results) {
+		if ( rows.length > 0 )  { 
+      			var rows_result = rows[0];
+			rows_result['serialNumber'];
+			rows_result_select = rows_result['serialNumber'];
+      			console.log('Resultat :' + rows_result_select);
+    		} else {
+	      		console.log("Pas de données");
+    		}
+		if (error){
+                         throw error;
+                }
+
+                console.log("Demande si la sonde est déjà en défault : ");
+ 
+		// Test si la sonde en question est déjà en défault
+		if(rows_result_select == serialNumber){
+			console.log("Le default existe déjà");
+			 // Requête pour mettre à jour le statut du capteur
+        		var sql = "UPDATE ?? SET ?? = 0 WHERE ?? = ?";
+        		var params = ['parc', 'status','serialNumber', serialNumber];
+        		sql = mysql.format(sql, params);
+
+        		connection.query(sql, function (error, results) {
+                	if (error) throw error;
+        		        console.log("Mise à jour de la table parc pour défaut " + message_str);
+	        	});
+
+		}else{
+			// Requête pour insérer une nouvelle ligne de défault dans la table parc
+        		var sql = "INSERT INTO ?? (??,??,??,??,??) VALUES (?,?,?,?,?)";
+        		var params = ['parc', 'topic','serialNumber','type','dateTimeProduction', 'status', topic, serialNumber, type, dAujourdhui, status];
+        		sql = mysql.format(sql, params);
+
+        		connection.query(sql, function (error, results) {
+                	if (error) throw error;
+                		console.log("Message ajouté dans la table de gestion de parc : ");
+        		});	
+		}
+	});
+};
+
+
 
 //split a string into an array of substrings
 function extract_string(message_str) {
-	var message_arr = message_str.split(","); //convert to array	
+	var message_arr = message_str.split(","); 	
 	return message_arr;
 };	
 
